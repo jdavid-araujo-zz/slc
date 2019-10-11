41c02d8612fd63e9ede08c5eaf7a882bbf709b05
jdavid-araujo <j.davidsousaaraujo@gmail.com>
2019-10-11 14:57:15 -0300

    Remoção de warnings

--- src/main/java/com/david/slc/resource/SlcResource.java
+++ src/main/java/com/david/slc/resource/SlcResource.java
@@ -10,20 +10,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.david.slc.model.Slc;
-import com.david.slc.service.FileService;
 import com.david.slc.service.SlcService;
 
 @RestController
 @RequestMapping("/slc")
 public class SlcResource implements Serializable {
+
+	private static final long serialVersionUID = 8213536130274145863L;
 	
 	private SlcService slcService;
-	private FileService FileService;
 	
 	@Autowired
-	public SlcResource(SlcService slcService, FileService fileService) {
+	public SlcResource(SlcService slcService) {
 		this.slcService = slcService;
-		this.FileService = fileService;
 	}
 
 	@GetMapping
